c6c32711034419a523f939fc15d24c52ddd74772
董飞祥 <fdong26@gmail.com>
2023-02-25 10:28:48 +0800

    规范化

--- source/js/main.js
+++ source/js/main.js
@@ -1,150 +1,158 @@
-// 菜单
-$(".menu-switch").click(function () {
-    if ($(this).hasClass('icon-menu-outline')) {
-        $(this).removeClass(' icon-menu-outline ').addClass('icon-close-outline');
-        $('.menu-container').css('opacity', '1').css('height', 'auto');
-    } else {
-        $(this).addClass(' icon-menu-outline ').removeClass('icon-close-outline');
-        $('.menu-container').css('opacity', '0');
+/* global $, window, document */
+'use strict';
 
-        var that = $(this);
-        setTimeout(function () {that.hasClass('icon-menu-outline') && $('.menu-container').css('height', '0')}, 600)
-    }
+// 菜单
+$('.menu-switch').click(function() {
+  if ($(this).hasClass('icon-menu-outline')) {
+    $(this).removeClass(' icon-menu-outline ').addClass('icon-close-outline');
+    $('.menu-container').css('opacity', '1').css('height', 'auto');
+  } else {
+    $(this).addClass(' icon-menu-outline ').removeClass('icon-close-outline');
+    $('.menu-container').css('opacity', '0');
+
+    const that = $(this);
+    setTimeout(() => { that.hasClass('icon-menu-outline') && $('.menu-container').css('height', '0'); }, 600);
+  }
 });
 
 if (window.is_post) {
-    // 图片放大
-    // $(".post-detail img").each(function() {
-    //     var currentImage = $(this);
-    //     currentImage.wrap("<a href='" + currentImage.attr("src") + "' data-fancybox='lightbox' data-caption='" + currentImage.attr("alt") + "'></a>");
-    // });
+  // 图片放大
+  // $(".post-detail img").each(function() {
+  //     var currentImage = $(this);
+  //     currentImage.wrap("<a href='" + currentImage.attr("src") + "' data-fancybox='lightbox' data-caption='" + currentImage.attr("alt") + "'></a>");
+  // });
 
-    // 代码复制
-    var $copyIcon = $('<i class="fa-solid icon icon-copy copy-code" title="复制代码"></i>');
-    $(".post-detail figure").append($copyIcon);
-    $(".post-detail pre[class*=language-].line-numbers").append($copyIcon);
-    $('.post-detail .copy-code').on('click', function () {
-        var selection = window.getSelection();
-        var range = document.createRange();
-        var table = $(this).prev('table');
-        if (table.length) {
-            range.selectNodeContents($(this).prev('table').find('.code').find('pre')[0]);
+  // 代码复制
+  const $copyIcon = $('<i class="fa-solid icon icon-copy copy-code" title="复制代码"></i>');
+  $('.post-detail figure').append($copyIcon);
+  $('.post-detail pre[class*=language-].line-numbers').append($copyIcon);
+  $('.post-detail .copy-code').on('click', function() {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    const table = $(this).prev('table');
+    if (table.length) {
+      range.selectNodeContents($(this).prev('table').find('.code').find('pre')[0]);
+    } else {
+      console.log($(this).prev('code')[0]);
+      range.selectNodeContents($(this).prev('code')[0]);
+    }
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+    selection.toString();
+    document.execCommand('copy');
+    selection.removeAllRanges();
+
+    $(this).html('<span class="copy-success"> 复制成功</span>');
+    setTimeout(() => {
+      $(this).html('');
+    }, 2500);
+  });
+
+  // 代码语言
+  $(() => {
+    $('code').each(function() {
+      const code_language = $(this).attr('class');
+
+      if (!code_language) {
+        return true;
+      }
+      const lang_name = code_language.replace('line-numbers', '').trim().replace('highlight', '').trim().replace('language-', '').trim();
+
+      $(this).attr('data-content-after', lang_name || 'CODE');
+    });
+    $('.highlight').each(function() {
+      const code_language = $(this).attr('class');
+
+      if (!code_language) {
+        return true;
+      }
+      const lang_name = code_language.replace('highlight', '').trim();
+
+      $(this).attr('data-content-after', lang_name || 'CODE');
+    });
+  });
+
+  // 文章详情侧边目录
+  const mainNavLinks = document.querySelectorAll('.top-box a');
+  window.addEventListener('scroll', event => {
+    const fromTop = window.scrollY + 100;
+
+    mainNavLinks.forEach((link, index) => {
+      const section = document.getElementById(decodeURI(link.hash).substring(1));
+      let nextSection = null;
+      if (mainNavLinks[index + 1]) {
+        nextSection = document.getElementById(decodeURI(mainNavLinks[index + 1].hash).substring(1));
+      }
+      if (section.offsetTop <= fromTop) {
+        if (nextSection) {
+          if (nextSection.offsetTop > fromTop) {
+            link.classList.add('current');
+          } else {
+            link.classList.remove('current');
+          }
         } else {
-            console.log($(this).prev('code')[0]);
-            range.selectNodeContents($(this).prev('code')[0]);
+          link.classList.add('current');
         }
-
-        selection.removeAllRanges();
-        selection.addRange(range);
-        var text = selection.toString();
-        document.execCommand('copy');
-        selection.removeAllRanges();
-
-        $(this).html('<span class="copy-success"> 复制成功</span>');
-        setTimeout(() => {
-            $(this).html('');
-        }, 2500)
+      } else {
+        link.classList.remove('current');
+      }
     });
+  });
 
-    // 代码语言
-    $(function () {
-        $('code').each(function () {
-            var code_language = $(this).attr('class');
+  // 点击锚点滚动条偏移
+  $('.top-box-link').click(() => {
+    setTimeout(() => {
+      $(window).scrollTop($(window).scrollTop() - 54);
+      console.log($(window).scrollTop() - 54, '滚动条位置');
+    }, 0);
+  });
+}
 
-            if (!code_language) {
-                return true;
-            }
-            var lang_name = code_language.replace("line-numbers", "").trim().replace("highlight", "").trim().replace("language-", "").trim();
+function lazyload(imgs, data) {
+  data.now = Date.now();
+  data.needLoad = Array.from(imgs).some(i => i.hasAttribute('lazyload'));
 
-            $(this).attr('data-content-after', lang_name || 'CODE');
-        });
-        $('.highlight').each(function () {
-            var code_language = $(this).attr('class');
+  const h = window.innerHeight;
+  const s = document.documentElement.scrollTop || document.body.scrollTop;
 
-            if (!code_language) {
-                return true;
-            }
-            var lang_name = code_language.replace("highlight", "").trim();
+  imgs.forEach(img => {
+    if (img.hasAttribute('lazyload') && !img.hasAttribute('loading')) {
 
-            $(this).attr('data-content-after', lang_name || 'CODE');
-        });
-    });
-
-    // 文章详情侧边目录
-    let mainNavLinks = document.querySelectorAll(".top-box a");
-    window.addEventListener("scroll", event => {
-        let fromTop = window.scrollY + 100;
-
-        mainNavLinks.forEach((link, index) => {
-            let section = document.getElementById(decodeURI(link.hash).substring(1));
-            let nextSection = null
-            if (mainNavLinks[index + 1]) {
-                nextSection = document.getElementById(decodeURI(mainNavLinks[index + 1].hash).substring(1));
-            }
-            if (section.offsetTop <= fromTop) {
-                if (nextSection) {
-                    if (nextSection.offsetTop > fromTop) {
-                        link.classList.add("current");
-                    } else {
-                        link.classList.remove("current");
-                    }
-                } else {
-                    link.classList.add("current");
-                }
-            } else {
-                link.classList.remove("current");
-            }
-        });
-    });
-
-    // 点击锚点滚动条偏移
-    $(".top-box-link").click(function(){
-        setTimeout(function () {
-            $(window).scrollTop($(window).scrollTop() - 54);
-            console.log($(window).scrollTop() - 54, '滚动条位置');
-        }, 0);
-    });
+      if ((h + s) > img.offsetTop) {
+        img.setAttribute('loading', true);
+        const loadImageTimeout = setTimeout(() => {
+          // eslint-disable-next-line no-undef
+          const temp = new Image();
+          const src = img.getAttribute('data-src');
+          temp.src = src;
+          temp.onload = () => {
+            img.src = src;
+            img.removeAttribute('lazyload');
+            img.removeAttribute('loading');
+            clearTimeout(loadImageTimeout);
+          };
+        }, 500);
+      }
+    }
+  });
 }
 
 // 图片懒加载
 if (window.theme_config.image && window.theme_config.image.lazyload_enable) {
-    const imgs = document.querySelectorAll('img');
-    let now = Date.now();
-    let needLoad = true;
+  const imgs = document.querySelectorAll('img');
 
-    function lazyload(imgs) {
-        now = Date.now();
-        needLoad = Array.from(imgs).some(i => i.hasAttribute('lazyload'));
+  const data = {
+    now: Date.now(),
+    needLoad: true
+  };
 
-        const h = window.innerHeight;
-        const s = document.documentElement.scrollTop || document.body.scrollTop;
 
-        imgs.forEach(img => {
-            if (img.hasAttribute('lazyload') && !img.hasAttribute('loading')) {
+  lazyload(imgs, data);
 
-                if ((h + s) > img.offsetTop) {
-                    img.setAttribute('loading', true);
-                    const loadImageTimeout = setTimeout(() => {
-                        const temp = new Image();
-                        const src = img.getAttribute('data-src');
-                        temp.src = src;
-                        temp.onload = () => {
-                            img.src = src;
-                            img.removeAttribute('lazyload');
-                            img.removeAttribute('loading');
-                            clearTimeout(loadImageTimeout);
-                        }
-                    }, 500);
-                }
-            }
-        });
+  window.onscroll = () => {
+    if (Date.now() - data.now > 50 && data.needLoad) {
+      lazyload(imgs, data);
     }
-
-    lazyload(imgs);
-
-    window.onscroll = () => {
-        if (Date.now() - now > 50 && needLoad) {
-            lazyload(imgs);
-        }
-    }
+  };
 }
